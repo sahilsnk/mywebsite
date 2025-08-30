b937d09404922aa2ed41ebf4892b836ec6da086a
sahilsanaik <sahilsantoshnaik3112004wed@gmail.com>
2025-08-31 04:11:49 +0530

    my changes

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,7 @@ export default function Home() {
           Welcome to <span className="text-blue-600">Sahil&apos;s Website</span>
         </h1>
         <p className="text-lg sm:text-xl text-gray-600 dark:text-gray-300 max-w-xl mb-8">
-          This is my personal space on the internet. Exciting projects and ideas, I'll be adding my portfolio
-          coming soon 🚀
+          {"This is my personal space on the internet. Exciting projects and ideas, I'll be adding my portfolio coming soon 🚀"}
         </p>
 
         {/* Call-to-Actions */}
